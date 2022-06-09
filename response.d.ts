c77c88a81f9ec3f6ae91069e7500b393f715a766
Alan Liang <symb@olic.link>
2022-06-10 00:28:17 +0800

    feat(node): proper packaging of C++ APIs
    Previously, the C++ API is only available by passing the arguments
    as an array of strings. This is ugly and insecure. Dev experience
    sucks. This commit introduces a new API by wrapping each command
    as a JS function, and uses JS objects to pass the arguments. A
    TypeScript declaration is included, though it is suboptimal as all
    commands return the Response sum type.
    
    Example:
    
    - Old API: ticket([ 'add_user', '-u', 'yyu' ])
    - New API: ticket.addUser({ username: 'yyu' })

---
+++ response.d.ts
@@ -0,0 +1,36 @@
+interface Success {
+  success: true
+}
+interface User {
+  username: string
+  name: string
+  email: string
+  privilege: number
+}
+interface Train {
+  // TODO
+}
+interface BuyTicketSuccess {
+  status: 'success'
+  price: number
+}
+interface BuyTicketEnqueued {
+  status: 'enqueued'
+}
+type BuyTicketResponse = BuyTicketSuccess | BuyTicketEnqueued
+interface Order {
+  id: number
+  trainId: string
+  from: Date
+  to: Date
+  price: number
+  seats: number
+}
+
+export type Response =
+  Success |
+  User |
+  Train |
+  Train[] |
+  BuyTicketResponse |
+  Order[]
