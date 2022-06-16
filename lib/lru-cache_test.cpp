3377c997b077bf89f63ae536ae0c18511dc70a80
Alan Liang <symb@olic.link>
2022-06-16 21:04:17 +0800

    feat(lib): write cache for the lru cache
    Previously, the LRU cache did not cache write operations. Data is
    written to the file when the set function is executed. This caused
    a lot of unnecessary writes, especially in hot spots. This commit
    changes the LRU cache so that the data is only written to the file
    when the entry is cleared from the cache.

--- lib/lru-cache_test.cpp
+++ lib/lru-cache_test.cpp
@@ -3,32 +3,40 @@
 #include <assert.h>
 #include <string.h>
 
-template <typename Key, int kSize>
-using Cache = ticket::LruCache<Key, kSize>;
+bool called = false;
+auto handler (int key, char *buf, int length) -> void {
+  called = true;
+}
+
+template <typename Key, int kSize, typename Callback>
+using Cache = ticket::LruCache<Key, kSize, Callback>;
 
 auto main () -> int {
-  Cache<int, 4> c;
+  Cache<int, 4, decltype(&handler)> c{handler};
   assert(!c.get(0));
   char tmp[10];
   strncpy(tmp, "hello", sizeof(tmp));
-  assert(c.upsert(0, tmp, 6));
-  assert(!c.upsert(0, tmp, 6));
-  assert(!c.upsert(0, tmp, 6));
-  assert(c.upsert(1, tmp, 6));
-  assert(c.upsert(2, tmp, 6));
+  c.upsert(0, tmp, 6);
+  c.upsert(1, tmp, 6);
+  c.upsert(2, tmp, 6);
+  c.upsert(2, tmp, 6);
   assert(strncmp((char *) *c.get(0), "hello", 6) == 0);
   strncpy(tmp, "world", sizeof(tmp));
   assert(strncmp((char *) *c.get(0), "hello", 6) == 0);
-  assert(c.upsert(0, tmp, 6));
+  c.upsert(0, tmp, 6);
   assert(strncmp((char *) *c.get(0), "world", 6) == 0);
   assert(strncmp((char *) *c.get(1), "hello", 6) == 0);
-  assert(!c.upsert(0, tmp, 6));
-  assert(c.upsert(3, tmp, 6));
-  assert(c.upsert(4, tmp, 6));
+  c.upsert(0, tmp, 6);
+  c.upsert(3, tmp, 6);
+  assert(!called);
+  c.upsert(4, tmp, 6);
+  assert(called);
+  called = false;
   assert(!c.get(2));
   c.remove(4);
   assert(!c.get(4));
-  assert(c.upsert(5, tmp, 6));
+  c.upsert(5, tmp, 6);
+  assert(!called);
   assert(c.get(0) && c.get(1) && c.get(3));
   return 0;
 }
