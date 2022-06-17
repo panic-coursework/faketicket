0f58375965ee4fc002cd80fea06d086392906155
Alan Liang <symb@olic.link>
2022-06-17 10:57:28 +0800

    feat(datetime): datetime operators
    This commits adds comparison operators (>, <=, >=, ==, !=) to
    datetime classes and increment/decrement (++, --) to Date.

--- lib/datetime.h
+++ lib/datetime.h
@@ -4,13 +4,15 @@
 
 #include <iostream>
 
+#include "utility.h"
+
 namespace ticket {
 
 /**
  * @brief Class representing a date between 2021-06-01 and
  * 2021-08-31 (inclusive).
  */
-class Date {
+class Date : public Comparable<Date> {
  public:
   Date () = default;
   Date (int month, int date);
@@ -37,12 +39,32 @@ class Date {
    * (06-04 - 3 == 06-01)
    */
   auto operator- (int dt) const -> Date;
+  auto operator++ () -> Date & {
+    ++days_;
+    return *this;
+  }
+  auto operator++ (int) -> Date {
+    Date res = *this;
+    ++days_;
+    return res;
+  }
+  auto operator-- () -> Date & {
+    --days_;
+    return *this;
+  }
+  auto operator-- (int) -> Date {
+    Date res = *this;
+    --days_;
+    return res;
+  }
   /**
    * @brief calculates the difference between two Dates.
    * (06-04 - 06-01 == 3)
    */
   auto operator- (Date rhs) const -> int;
-  auto operator< (const Date &rhs) const -> bool;
+  auto cmp (Date rhs) const -> int {
+    return *this - rhs;
+  }
   /// checks if this Date is in the given range (inclusive).
   auto inRange (Date begin, Date end) const -> bool;
  private:
@@ -60,7 +82,7 @@ class Date {
  * 02:10” is a duration, while 02:10 as in “it's 02:10 now,
  * go to sleep right now” is an instant.
  */
-class Duration {
+class Duration : public Comparable<Duration> {
  public:
   Duration () = default;
   explicit Duration (int minutes) : minutes_(minutes) {}
@@ -70,7 +92,9 @@ class Duration {
   auto operator- (Duration dt) const -> Duration;
   /// negates the Duration.
   auto operator- () const -> Duration;
-  auto operator< (const Duration &rhs) const -> bool;
+  auto cmp (Duration rhs) const -> int {
+    return minutes_ - rhs.minutes_;
+  }
  private:
   int minutes_ = 0;
 };
@@ -83,7 +107,7 @@ class Duration {
  *
  * Not to be confused with Duration, see notes in Duration.
  */
-class Instant {
+class Instant : public Comparable<Instant> {
  public:
   Instant () = default;
   Instant (int hour, int minute);
@@ -97,7 +121,9 @@ class Instant {
   auto operator+ (Duration dt) const -> Instant;
   auto operator- (Duration dt) const -> Instant;
   auto operator- (Instant rhs) const -> Duration;
-  auto operator< (const Instant &rhs) const -> bool;
+  auto cmp (Instant rhs) const -> int {
+    return minutes_ - rhs.minutes_;
+  }
  private:
   explicit Instant (int minutes) : minutes_(minutes) {}
   int minutes_ = 0;
